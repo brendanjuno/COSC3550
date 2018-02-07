c2f777814e4a618c81ba9024322e1fdc979d6958
Brendan <brendan.juno@marquette.edu>
2018-02-07 10:07:06 -0600

    Initial commit

---
+++ DustGame/src/Dust.java
@@ -0,0 +1,49 @@
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+
+import java.util.Random;
+
+/**
+ * @author Brendan Juno
+ * @version 2/5/2018
+ *          Project: ComputerGames
+ *          Comments: Testing Git
+ */
+public class Dust {
+    int x;
+    int y;
+    int size = 20;
+    int vx;
+    int vy;
+    Random random = new Random();
+    public Dust(){
+        x = random.nextInt(500);
+        y = random.nextInt(500);
+        vx = -5+random.nextInt(10);
+        vy = -5+random.nextInt(10);
+    }
+
+    void render(GraphicsContext gc){
+        tranfer();
+        gc.setFill(Color.GREY);
+        gc.fillOval(x,y,size,size);
+    }
+
+    void tranfer(){
+        if (y<=0){
+            y = 495;
+            //x = 500-x;
+        }
+        if (y>=500){
+            y = 5;
+            //x = 500-x;
+        }
+        if (x<=-20){
+            x = 495;
+        }
+        if (x>=500){
+            x = 5;
+        }
+    }
+
+}
---
+++ DustGame/src/DustGame.java
@@ -0,0 +1,114 @@
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.paint.ImagePattern;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ * @author Brendan Juno and Jason W.
+ * @version 2/5/2018
+ *          Project: ComputerGames
+ *          Comments:
+ */
+public class DustGame extends Application{
+    String appName = "Call of Dusty: Modern Roomba 2";
+    Image carpet = new Image("Carpet.jpg");
+    final int WIDTH = 500;
+    final int HEIGHT = 500;
+    final int FPS = 30;
+    ArrayList<Dust> dustbunnies = new ArrayList<>();
+    Random random = new Random();
+    ROOMBA_STATE state = ROOMBA_STATE.LEFT;
+    private enum ROOMBA_STATE {LEFT,RIGHT,IDLE};
+
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage theStage) {
+        theStage.setTitle(appName);
+        int size = 10+random.nextInt(10);
+        System.out.println(size);
+        for (int i = 0;i<size;i++){
+            dustbunnies.add(new Dust());
+        }
+        Group root = new Group();
+        Scene theScene = new Scene(root);
+        theStage.setScene(theScene);
+
+        Canvas canvas = new Canvas(WIDTH, HEIGHT);
+        root.getChildren().add(canvas);
+        theScene.setOnKeyPressed(
+                new EventHandler<KeyEvent>()
+                {
+                    public void handle(KeyEvent e)
+                    {
+                        String code = e.getCode().toString();
+                        if (code.equals("LEFT"))
+                            state = ROOMBA_STATE.LEFT;
+                        if (code.equals("RIGHT"))
+                            state = ROOMBA_STATE.RIGHT;
+                    }
+                });
+
+        theScene.setOnKeyReleased(
+                new EventHandler<KeyEvent>()
+                {
+                    public void handle(KeyEvent e)
+                    {
+                        String code = e.getCode().toString();
+                        System.out.println(code);
+                        if (code.equals("LEFT")||code.equals("RIGHT"))
+                            state = ROOMBA_STATE.IDLE;
+                    }
+                });
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+
+
+        // Setup and start animation loop (Timeline)
+        KeyFrame kf = new KeyFrame(Duration.millis(1000 / FPS),
+                e -> {
+                    // update position
+                    update();
+                    // draw frame
+                    render(gc);
+                }
+        );
+        Timeline mainLoop = new Timeline(kf);
+        mainLoop.setCycleCount(Animation.INDEFINITE);
+        mainLoop.play();
+
+        theStage.show();
+    }
+
+    void render(GraphicsContext gc){
+        gc.setFill(new ImagePattern(carpet));
+        gc.fillRect(0, 0, WIDTH, HEIGHT);
+
+        for (Dust e : dustbunnies){
+            e.render(gc);
+        }
+    }
+
+    void update(){
+        for (Dust e: dustbunnies) {
+            e.x+=e.vx;
+            e.y+=e.vy;
+        }
+    }
+}
