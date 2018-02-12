35769b29221334f812e11859fcdfa8f44138cd13
Brendan <brendan.juno@marquette.edu>
2018-02-12 12:46:23 -0600

    Updated Explosion Graphics Added Game Over Screen Scoring Update Scoreboard now appears over everything

--- DustGame/src/DustGame.java
+++ DustGame/src/DustGame.java
@@ -14,11 +14,9 @@ import javafx.scene.paint.ImagePattern;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontPosture;
 import javafx.scene.text.FontWeight;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
-import java.text.DecimalFormat;
 import java.text.NumberFormat;
 import java.util.ArrayList;
 import java.util.Random;
@@ -41,7 +39,7 @@ public class DustGame extends Application{
     Random random = new Random();
     ROOMBA_STATE state = ROOMBA_STATE.IDLE;
     private enum ROOMBA_STATE {LEFT,RIGHT,IDLE}
-    int killcount = 0;
+    int score = 0;
     double frames = 0;
     int size;
     int wave = 1;
@@ -53,10 +51,13 @@ public class DustGame extends Application{
     int boostduration = 200;
     int explosionduration = 90;
     int catduration = 500;
+    int endduration = 150;
+    int finalScore;
     boolean bombhit = false;
     boolean nextwave = false;
     boolean speedhit = false;
     boolean cathit = false;
+    boolean gameend = false;
 
     String[] lives = {"dead.png", "low.png", "medium.png", "full.png"};
 
@@ -129,36 +130,14 @@ public class DustGame extends Application{
         }
         roomba.render(gc);
 
-        //Scoreboard Rendering
-        gc.setFill(Color.WHITE);
-        gc.fillRect(0,500,500,200);
-        gc.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 20));
-        frames++;
-        NumberFormat nf = NumberFormat.getInstance();
-        nf.setMaximumFractionDigits(1);
-        nf.setGroupingUsed(false);
-        gc.setFill(Color.BLACK);
-        gc.fillText("Time: "+nf.format(frames/FPS),20,525);
-        gc.fillText("Score: "+killcount,20,545);
-        gc.fillText("Bunnies Remaining: "+dustbunnies.size(),20,565);
-        gc.fillText("Wave: "+wave,20,585);
-        gc.fillText("Battery: " + roomba.battery, 375, 517);
-        gc.setFill(new ImagePattern(new Image(lives[roomba.battery])));
-        gc.fillRect(400, 520, 25, 75);
-        if(roomba.battery == 0){
-            System.exit(0);
-        }
-
-
         //Bomb render
         if(bomb!=null){
             bomb.render(gc);
             if (bombhit == true){
                 if (explosionduration>0){
-                    gc.setFill(new ImagePattern(new Image("exp.gif")));
-                    gc.fillRect(bomb.x-100,bomb.y-100,200,300);
+                    gc.setFill(new ImagePattern(new Image("boom.png")));
+                    gc.fillRect(bomb.x-100,bomb.y-100,300,150);
                     explosionduration--;
-
                 }else{
                     bomb = null;
                     bombduration = 300;
@@ -201,6 +180,42 @@ public class DustGame extends Application{
                 }
             }
         }
+
+        //Scoreboard Rendering
+        gc.setFill(Color.WHITE);
+        gc.fillRect(0,500,500,200);
+        gc.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 20));
+        frames++;
+        NumberFormat nf = NumberFormat.getInstance();
+        nf.setMaximumFractionDigits(1);
+        nf.setGroupingUsed(false);
+        gc.setFill(Color.BLACK);
+        gc.fillText("Time: "+nf.format(frames/FPS),20,525);
+        gc.fillText("Score: "+ score,20,545);
+        gc.fillText("Bunnies Remaining: "+dustbunnies.size(),20,565);
+        gc.fillText("Wave: "+wave,20,585);
+        gc.fillText("Battery: " + roomba.battery, 375, 517);
+        gc.setFill(new ImagePattern(new Image(lives[roomba.battery])));
+        gc.fillRect(400, 520, 25, 75);
+
+
+        //Game over render
+        if (roomba.battery == 0){
+            if (endduration<0){
+                System.exit(2);//Exit Game
+            }
+            if (!gameend){
+                finalScore = score;
+                gameend = true;
+            }
+            //Draw End Scene
+            gc.setFill(Color.WHITE);
+            gc.fillRect(0,0,WIDTH,HEIGHT);
+            gc.setFill(Color.BLACK);
+            gc.fillText("Game Over!",150,300);
+            gc.fillText("Final Score: "+finalScore,150,350);
+            endduration--;
+        }
     }
 
     void update(){
@@ -209,7 +224,7 @@ public class DustGame extends Application{
         for (int i = 0; i<dustbunnies.size();i++) {
             dustbunnies.get(i).move();
             if (((Math.pow(dustbunnies.get(i).cx-roomba.cx,2))+(Math.pow(roomba.cy-dustbunnies.get(i).cy,2)))<=(Math.pow((roomba.size/2)+(dustbunnies.get(i).size/2),2))){
-                killcount+=100;
+                score +=100;
                 dustbunnies.remove(i);
 
             }
@@ -252,7 +267,7 @@ public class DustGame extends Application{
                 int by = bomb.y+(bomb.size/2);
                 if (((Math.pow(bx-roomba.cx,2))+(Math.pow(roomba.cy-by,2)))<=(Math.pow((roomba.size/2)+(bomb.size/2),2))) {
                     if (nextwave==false){
-                        killcount += 50;
+                        score += 50;
                         dustbunnies.removeAll(dustbunnies);
                     }
                     nextwave = true;
